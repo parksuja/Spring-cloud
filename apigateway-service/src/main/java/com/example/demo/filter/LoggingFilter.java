7f4cb19ff4193b386eb084adc4220b30186e9df3
parksuja <njoy4089@nate.com>
2021-09-23 17:08:47 +0900

    09-23 apigateway-service

---
+++ apigateway-service/src/main/java/com/example/demo/filter/LoggingFilter.java
@@ -0,0 +1,85 @@
+package com.example.demo.filter;
+
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.OrderedGatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.core.Ordered;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Mono;
+
+@Component
+@Slf4j
+public class LoggingFilter extends AbstractGatewayFilterFactory<LoggingFilter.Config> {
+
+
+// 설정 정보를 제공하는 클래스
+
+	@Data
+	public static class Config {
+		private String baseMessage;
+		private boolean preLogger;
+		private boolean postLogger;
+		}
+
+
+public LoggingFilter() {
+
+super(Config.class);
+
+}
+
+
+// 필터의 동작을 정의한 메서드
+
+@Override
+
+public GatewayFilter apply(Config config) {
+
+GatewayFilter filter = null;
+
+filter = new OrderedGatewayFilter((exchange, chain) -> {
+
+// 요청이 전달되었을 때 요청 아이디를 로그로 출력
+
+ServerHttpRequest request = exchange.getRequest();
+
+ServerHttpResponse response = exchange.getResponse();
+
+
+log.info("Logging Filter baseMessage = {}", config.getBaseMessage());
+
+
+if (config.isPreLogger()) {
+
+log.info("Logging Filter Start ... request id = {}", request.getId());
+
+}
+
+
+// custom post filter
+
+// 응답의 처리상태코드를 로그로 출력
+
+return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+
+if (config.isPostLogger()) {
+
+log.info("Logging Filter End ... status code = {}", response.getStatusCode());
+
+}
+
+}));
+
+}, Ordered.LOWEST_PRECEDENCE);
+
+return filter;
+
+}
+
+
+}
